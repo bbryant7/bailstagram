a1fd317c547b9bda3dc4d74293ca4622ae01b773
Bailey Bryant <bailey.bryant10@gmail.com>
2017-11-17 09:49:28 -0600

    initial commit

---
+++ client/reduxstagram.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+// Import css
+import css from './styles/style.styl';
+// Import Components
+import Main from './components/Main';
+import Single from './components/SinglePost';
+import PhotoGrid from './components/Grid';
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+
+
+render(<main />, document.getElementById('root'));
