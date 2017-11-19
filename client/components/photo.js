a3b8c8f6e2717b7e0c22786b7d42355a1a39a595
Bailey Bryant <bailey.bryant10@gmail.com>
2017-11-18 21:00:12 -0600

    render image, like buttons and comments button

--- client/components/photo.js
+++ client/components/photo.js
@@ -7,9 +7,23 @@ const Photo = React.createClass({
       <figure className="grid-figure">
       <div className="grid-photo-wrap">
       <Link to={`/view/${this.props.post.code}`}>
-      {this.props.post.caption}
+        <img src={this.props.post.display_src} alt={this.props.post.caption} className="grid-photo"/>
       </Link>
       </div>
+
+      <figcaption>
+      <p> {this.props.post.caption}</p>
+      <div className="control-buttons">
+      <button className="likes"> &hearts; {this.props.post.likes}</button>
+      <Link className="button" to={`/view/{this.props.post.code}`}>
+          <span className="comment-count">
+          <span className="speech-bubble"></span>
+          {this.props.comments[this.props.post.code] ? this.props.comments[this.props.post.code].length: 0}
+        </span>
+        </Link>
+
+      </div>
+      </figcaption>
       </figure>
     )
   }
